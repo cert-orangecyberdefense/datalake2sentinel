39759365dd95d781d0af10892132ce52ab7be12f
paul.dumitru <paul.dumitru@orange.com>
2026-03-20 15:03:17 +0200

    PEER-127: Allow disabling the log file

--- AzureFunction/Datalake2Sentinel/logger.py
+++ AzureFunction/Datalake2Sentinel/logger.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import RotatingFileHandler
 import os
 import sys
 
@@ -12,12 +13,6 @@ class Logger:
         if getattr(config, "verbose_log", False):
             logger.setLevel(logging.DEBUG)
 
-        file_handler = logging.FileHandler(
-            os.getenv("log_file", "/tmp/datalake2sentinel.log"), mode="a"
-        )
-        file_handler.setLevel(logging.INFO)
-        if getattr(config, "verbose_log", False):
-            file_handler.setLevel(logging.DEBUG)
 
         stream_handler = logging.StreamHandler(sys.stderr)
         stream_handler.setLevel(logging.INFO)
@@ -27,10 +22,21 @@ class Logger:
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
-        file_handler.setFormatter(formatter)
         stream_handler.setFormatter(formatter)
 
-        logger.addHandler(file_handler)
+        log_file = os.getenv("log_file", None)
+
+        if log_file:
+            file_handler = RotatingFileHandler(
+                log_file , mode="a", maxBytes=16000, backupCount=1
+            )
+            file_handler.setLevel(logging.INFO)
+            if getattr(config, "verbose_log", False):
+                file_handler.setLevel(logging.DEBUG)
+
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
         logger.addHandler(stream_handler)
 
         return logger
